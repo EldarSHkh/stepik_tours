e5a5032bca4a360338ce7996a10983428f95da8c
Eldar <eldar.shkh@gmail.com>
2020-09-06 17:30:30 +0300

    Stepik_tours2

--- app_tours/views.py
+++ app_tours/views.py
@@ -1,32 +1,54 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
 from django.views import View
+
+import random
 
 from app_tours.data import departures, description, tours, title, subtitle
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
+        random_tour_id = random.sample(range(1, len(tours)), 6)
+        tour = {tour_id: tours[tour_id] for tour_id in random_tour_id}
         context = {
             "title": title,
             "subtitile": subtitle,
-            "description": description
+            "description": description,
+            'departures': departures,
+            'active': '',
+            "tours": tour,
+
         }
         return render(
             request, 'index.html', context=context)
 
 
 class DepartureView(View):
-    def get(self, request, departure_id):
-        if departure_id not in departures:
-            raise Http404
-        context = {
-            "departure": departures[departure_id]
-        }
 
-        return render(
-            request, 'departure.html', context=context
-        )
+    def get(self, request, departure):
+        if departure not in departures.keys():
+            raise Http404
+        departure_tours = {key: value for key, value in tours.items() if value['departure'] == departure}
+        list_tour = departure_tours.values()
+        max_cost = max(list_tour, key=lambda x: x['price'])
+        min_cost = min(list_tour, key=lambda x: x['price'])
+        max_nights = max(list_tour, key=lambda x: x['nights'])
+        min_nights = min(list_tour, key=lambda x: x['nights'])
+        context = {'title': title,
+                   'tour_title': departures[departure],
+                   'active': departure,
+                   'departure': departures[departure],
+                   'departure_tours': departure_tours,
+                   'tour_count': len(departure_tours),
+                   'max_cost': max_cost['price'],
+                   'max_nights': max_nights['nights'],
+                   'min_cost': min_cost['price'],
+                   'min_nights': min_nights['nights'],
+                   'departures': departures,
+                   }
+
+        return render(request, 'departure.html', context=context)
 
 
 class TourView(View):
@@ -34,9 +56,9 @@ class TourView(View):
         if id not in tours:
             raise Http404
 
-        title = tours[id]["title"]
+        tour_title = tours[id]["title"]
         description = tours[id]["description"]
-        departure = tours[id]["departure"]
+        departure = departures[tours[id]['departure']]
         picture = tours[id]["picture"]
         price = tours[id]["price"]
         stars = tours[id]["stars"]
@@ -45,6 +67,7 @@ class TourView(View):
         date = tours[id]["date"]
 
         context = {
+            "tour_title": tour_title,
             "title": title,
             "description": description,
             "departure": departure,
@@ -53,9 +76,19 @@ class TourView(View):
             "stars": stars,
             "country": country,
             "nights": nights,
-            "date": date
+            "date": date,
+            'active': departure,
+            'departures': departures
         }
 
         return render(
             request, 'tour.html', context=context
         )
+
+
+def my_handler404(request, exception):
+    return HttpResponseNotFound('404')
+
+
+def my_handler500(request):
+    return HttpResponseServerError('500')
--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib import admin
 
-from app_tours.views import MainView, DepartureView, TourView
-
+from app_tours.views import MainView, DepartureView, TourView, my_handler404, my_handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', MainView.as_view()),
-    path('departure/<str:departure_id>/', DepartureView.as_view()),
-    path('tour/<int:id>/', TourView.as_view()),
+    path('', MainView.as_view(), name='main'),
+    path('departure/<str:departure>/', DepartureView.as_view(), name='departure'),
+    path('tour/<int:id>/', TourView.as_view(), name='tour'),
 ]
+
+handler404 = my_handler404
+handler500 = my_handler500
